0d5c656561d1a5f23f3d1644ce10a2286ae1fe11
anirudhtopiwala <topiwala.anirudh@gmail.com>
2020-03-24 14:49:38 -0400

    Added longest substring without repeating chars

---
+++ LeetCode/3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ * Copyright (c) 2020 Anirudh Topiwala
+ * Author: Anirudh Topiwala
+ * Create Date: 2020-03
+ * Last Edit Date: 2020-03
+ *
+ * @brief: This problem is solved by keeping track of two pointers. The 'i' pointer
+ * is pointing towards a position before the start of the substring whereas the 'j' pointer 
+ * points to the last element of the substring. The repetition of characters are tracked
+ * by an unordered_pair which tracks the characters and their indices. The max length 
+ * of substring is tracked by max_count. The map updates 'i' pointer to the last seen location
+ * of the repeated character by also double checking that the new postion of 'i' is not 
+ * behind its current position.
+ * 
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+      const int sz = s.size();
+      if (sz == 0){
+        return 0;
+      } else if(sz == 1){
+        return 1;
+      }
+      int i = -1; int j =0;
+      int max_count = 1;
+      std::unordered_map<char, int> map;
+      while ( i < sz && j < sz){
+        if (map.find(s[j]) != map.end() && map[s[j]] >= i){
+            i = map[s[j]];  
+        }
+        max_count = std::max(max_count, j-i);
+        map[s[j]] = j;
+        ++j; 
+      }
+      return max_count;
+    }
+};
+
+int main() {
+  const std::vector<std::string> vec_str {"abcabcbb", "bbbbb", "pwwkew", "abba"}; 
+  Solution sol;
+  for (auto str : vec_str) {
+    std::cout<<sol.lengthOfLongestSubstring(str)<<std::endl;
+  }
+}
